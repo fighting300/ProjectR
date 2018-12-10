8bad005ca1c2225c93998dfd410e0e9c206b1684
Dragon <fighting300@163.com>
2018-12-10 14:17:12 +0800

    首页icon

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import MainNavigator from './Main';
 import Activity from './Activity';
 
@@ -43,6 +44,28 @@ const TabNavigator = createBottomTabNavigator(
   {
     initialRouteName: "Main",
     order: ['Main', 'Activity', 'Live', 'Cloud', 'Community'],
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Main') {
+          iconName = `newspaper-o`;
+        } else if (routeName === 'Activity') {
+          iconName = `book`;
+        } else if (routeName === 'Live') {
+          iconName = `eye`;
+        } else if (routeName === 'Cloud') {
+          iconName = `cloud`;
+        } else if (routeName === 'Community') {
+          iconName = `cube`;
+        } 
+        
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Icon name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+    }),
+
     tabBarOptions: {
       activeTintColor: '#2a5caa',
       activeBackgroundColor: '#fffef9',
